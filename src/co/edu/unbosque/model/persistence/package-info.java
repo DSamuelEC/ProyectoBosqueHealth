9881f5e3dd589908ab8a3dfa926024022ff0f273
Samuel Díaz <124406776+DSamuelEC@users.noreply.github.com>
2024-11-10 03:28:43 -0500

    ca

---
+++ src/co/edu/unbosque/model/persistence/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * paquete persistence encargado de contener toda la persistencia del programa,
+ * conteniendo clases, metodos, y una clase que permita manipular los archivos
+ * 
+ * @author Samuel Diaz
+ */
+package co.edu.unbosque.model.persistence;
